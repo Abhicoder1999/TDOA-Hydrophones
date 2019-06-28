fc6d37708bc159808dd132717cb905a15903be94
Abhijeet Tripathy <abhijeetofficial1999@gmail.com>
2019-06-28 13:08:46 +0530

    initial setup

---
+++ tdoa_code/build/CMakeFiles/exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exe.dir/src/Util.cpp.o"
+  "CMakeFiles/exe.dir/src/tdoa.cpp.o"
+  "exe.pdb"
+  "exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
